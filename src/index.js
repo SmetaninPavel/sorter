a1ab8b52f3a5f850eb5e7388536c8fd18ae845fe
Pavel <SmetaninPavel@users.noreply.github.com>
2018-09-18 15:35:04 +0300

    [03]  Smart sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,63 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.sortFunction = (a, b) => a - b;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    const elementsIndexesToSort = indices.sort((a, b) => a - b);
+
+    const toSortArrLength = elementsIndexesToSort.length;
+
+    if (toSortArrLength <= 1) {
+      return this.array;
+    }
+
+    if (Math.abs(elementsIndexesToSort[0] - elementsIndexesToSort[toSortArrLength - 1]) !== 1 && toSortArrLength === 2) {
+
+      const firstElement = this.array[elementsIndexesToSort[0]];
+      const lastElement = this.array[elementsIndexesToSort[toSortArrLength - 1]];
+
+      if (firstElement !== -1 && lastElement !== -1) {
+        this.array[elementsIndexesToSort[0]] = lastElement;
+        this.array[elementsIndexesToSort[toSortArrLength - 1]] = firstElement;
+      }
+
+      return this.array;
+    }
+
+    const elementsToSortInArr = this.array.slice(
+      elementsIndexesToSort[0],
+      elementsIndexesToSort[toSortArrLength - 1] + 1
+    );
+
+    this.array.splice(
+      elementsIndexesToSort[0],
+      toSortArrLength,
+      ...elementsToSortInArr.sort(this.sortFunction)
+    );
+
+    return this.array;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.sortFunction = compareFunction;
   }
 }
 
